6bffde61468dfe2c80a0116efa032a2509feda24
Akvis24 <akvis2022@gmail.com>
2026-05-28 06:54:37 +0200

    why me sektion angefangen

--- src/app/pages/mainpage/mainpage.ts
+++ src/app/pages/mainpage/mainpage.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Hero } from '../../components/hero/hero';
+import { WhyMe } from "../../components/why-me/why-me";
 
 @Component({
   selector: 'app-mainpage',
-  imports: [Hero],
+  imports: [Hero, WhyMe],
   templateUrl: './mainpage.html',
   styleUrl: './mainpage.scss',
 })
